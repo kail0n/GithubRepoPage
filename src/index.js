d5904b8cc60a7203879b46bc1bab050a8f97eed4
kail0n <kailontse@gmail.com>
2020-12-01 17:49:01 +0000

    create store

--- src/index.js
+++ src/index.js
@@ -2,19 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import store from './store';
+import { createStore } from 'redux';
+import searchReducer from './reducers/searchReducer';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+const store = createStore(searchReducer, devToolsEnhancer());
 
 import App from './App.js';
-// import './styles/index.css';
+// import './styles/index.css'
 
 ReactDOM.render(
     <React.StrictMode>
-        {/* <Provider store={store}> */}
-        {/* <Provider> */}
+        <Provider store={store}>
             <Router>
                 <App />
             </Router>
-        {/* </Provider> */}
+        </Provider>
     </React.StrictMode>,
     document.getElementById("root")
 );
--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -1,9 +1,13 @@
 const initState = { username: ""}
 
-const searchReducer = (state=initState, action) => {
+const searchReducer = (state=initState, action) =>{
     switch(action.type){
         case "ADD_A_USERNAME":
             const usernameToUpdate =  action.payload;
             return{username: usernameToUpdate}
+        default:
+            return state;
     }
 }
+
+export default searchReducer;
