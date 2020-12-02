60403aa260e126f2de8c3805a87a28703e449bbc
PaolaDM <orchidea.preziosa@gmail.com>
2020-12-02 21:24:46 +0000

    fixe api call and set state.userRepos values

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-import { fetchUserProfile } from '../../actions/searchAction.js';
- 
+// import { fetchUserProfile } from '../../actions/searchAction.js';
 
-let userRepos = [];
 class SearchForm extends Component {
-    state = {username:""}
+    state = {
+        username:"",
+        userRepos: []
+        }
 
-    handleSubmit = e => {
-        e.preventDefault();
-        const userRepos = fetchUserProfile(this.state.username)
-        console.log(userRepos)
+    // handleSubmit = e => {
+    //     e.preventDefault();
+    //     const inputValue = this.state.username;
+    //      async const try {
+    //         const resp = await fetch(`https://api.github.com/users/${inputValue}/repos`);
+    //         const data = await resp.json();
+    //         // if (data.status === 404) { throw Error(err.message) }
+    //         console.log("this is: ", data)
+    //         this.setState({ userRepos : data });   
+    //     } catch(err) {
+    //         throw new Error(err.message)
+    //     }
+        
+        handleSubmit = e => {
+            e.preventDefault();
+            const inputValue = this.state.username;
+                fetch(`https://api.github.com/users/${inputValue}/repos`)
+                    .then(data => data.json())
+                    .then((json)=>{this.setState({userRepos: json})})
+                    .catch((err) => {
+                        console.error(err.message)
+                    })
         }
 
     handleInputChange = e => {
@@ -17,20 +36,23 @@ class SearchForm extends Component {
         this.setState({ username }) 
         }
 
+    
+        
     render() {
+    
         return(
-            <form onSubmit={this.handleSubmit}>
-                <input id="textInput" type="text" placeholder="Enter your username" value={this.username} onChange={this.handleInputChange}></input>
-                <input type="submit" value="Search User"></input>
-                <div> {userRepos.map((repo) => {
-                  return (<li key={repo}>{repo}</li>)})
-                  }
-                </div>
-            </form>
+            <>
+                <form onSubmit={this.handleSubmit}>
+                    <input id="textInput" type="text" placeholder="Enter your username" value={this.username} onChange={this.handleInputChange}></input>
+                    <input type="submit" value="Search User"></input>
+                </form>
+                {/* <div> {this.state.userRepos}</div> */}
+            </>
         )
     }
     
 }
+
 
 
 export default (SearchForm)
